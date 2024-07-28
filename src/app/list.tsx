065f1ac9ea1e87f1b36743b086015873e5f15242
RaphaPetrere <raphaelpetrere@gmail.com>
2024-07-27 21:19:21 -0300

    Realizando uma espécie de listagem em stack de cartões e ja deixando mockada a chamada para api para consumo dos cartões

--- src/app/list.tsx
+++ src/app/list.tsx
@@ -1,11 +1,122 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Pressable, FlatList } from 'react-native'
+import React, { useState } from 'react'
+import Container from '../components/Container'
+import { Link, Stack } from 'expo-router'
+import theme from '../theme'
+import { FontAwesome5 } from '@expo/vector-icons';
+import Body from '../components/Body'
+import Card, { CardProps } from '../components/Card'
 
 const List = () => {
+  const [cards, setCards] = useState<CardProps[]>([
+    {
+      "id": "4ec42ba9-50af-40d2-af90-8312edbd9ca2",
+      "number": "3529 5435 3355 8727",
+      "cvv": "317",
+      "name": "John Doe"
+    },
+    {
+      "id": "4ec42ba9-50af-40d2-af90-8312edbd9ca3",
+      "number": "3529 5435 3355 8727",
+      "cvv": "317",
+      "name": "John Doe"
+    },
+    {
+      "id": "4ec42ba9-50af-40d2-af90-8312edbd9ca4",
+      "number": "3529 5435 3355 8727",
+      "cvv": "317",
+      "name": "John Doe"
+    }
+  ]);
+  /* fetch('http://localhost:3000/cards')
+  .then((res) => res.json())
+  .then(response => setCards(response))
+  .catch(err => console.log('err', err)) */
   return (
-    <View>
-      <Text>List Screen</Text>
-    </View>
+    <Container showImages={false}>
+      <Stack.Screen
+        options={{
+          title: 'Wallet Test',
+          headerTitleStyle: {color: theme.background, fontSize: 22, fontWeight: '100'},
+          headerRight: () => (
+            <Link href='cadastro' asChild>
+              <Pressable 
+                android_ripple={{
+                  color: theme.gray,
+                  radius: 20,
+                  borderless: true
+                }}
+              >
+                <FontAwesome5 name='plus' size={16} color={theme.accent} />
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: theme.lighterGray,
+          borderBottomStartRadius: 48,
+          borderBottomEndRadius: 48,
+          padding: 16,
+        }}
+      >
+        <Text style={{color: theme.accent, fontSize: 20, textAlign: 'center'}}>
+          Meus Cartões
+        </Text>
+      </View>
+      <Body>
+        <View 
+          style={{
+            position: 'relative', 
+            width: 300, 
+            height: 200, 
+          }}
+        >
+          {cards.map((card, index) => (
+            <View key={card.id}
+              style={{
+                position: 'absolute',
+                bottom: (50*index),
+                elevation: 20 - (index * 4),
+                zIndex: cards.length - index,
+                shadowColor: theme.black,
+              }}
+            >
+              <Card card={card} variant={index % 2 === 0 ? 'black' : 'green'} />
+            </View>
+          ))}
+        </View>
+        <Text style={{color: theme.white, marginTop: 10, fontSize: 16}}>
+          usar este cartão
+        </Text>
+        {/* <FlatList 
+          data={cards}
+          keyExtractor={card => card.id}
+          renderItem={({item, index}) => (
+            <View key={item.id}
+              style={{
+                position: 'absolute',
+                bottom: (50*index),
+                left: 0,
+                elevation: cards.length - index,
+                zIndex: cards.length - index
+              }}
+            >
+              <Card card={item} />
+              <Text>Index {index}</Text>
+            </View>
+          )}
+          style={{
+            height: 200, 
+            width: 300, 
+            flexGrow: 0, 
+            position: 'relative', 
+            backgroundColor: 'red'
+          }}
+        /> */}
+      </Body>
+    </Container>
   )
 }
 
