8fe684fee5e7c6b6cf362e97b4b61381d56291c4
RaphaPetrere <raphaelpetrere@gmail.com>
2024-07-31 13:29:55 -0300

    Adicionando o uuid no moduleNameMapper para achar a declaração da biblioteca na hora de fazer o teste

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,8 @@ module.exports = {
   ],
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
-  ]
+  ],
+  moduleNameMapper: {
+    '^uuid$': '<rootDir>/node_modules/uuid/dist/index.js'
+  }
 }
