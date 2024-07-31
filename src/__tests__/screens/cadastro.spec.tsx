e94bbc7b9ba1cd988032f3329f07c6b337747dd6
RaphaPetrere <raphaelpetrere@gmail.com>
2024-07-31 14:39:08 -0300

    Adicionando mais testes para as funções utilizadas na tela de Cadastro

--- src/__tests__/screens/cadastro.spec.tsx
+++ src/__tests__/screens/cadastro.spec.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
 import CustomInput from '../../components/CustomInput';
+import { onlyNumbers } from '../../utils/onlyNumbers';
+import { addChar } from '../../utils/addChar';
 
 describe('Cadastro', () => {
   it('check if it shows correctly the card number input placeholder', () => {
@@ -9,4 +11,32 @@ describe('Cadastro', () => {
     const inputNumCartao = getByLabelText('número do cartão');
     expect(inputNumCartao).toBeTruthy();
   });
+
+  test('if handleNumCartao with valid digits filter all the non-numbers digits and add a space between each 4-digits block', () => {
+    const numWithoutSpaces = onlyNumbers('b352954a5335c58-727');
+    expect(addChar(numWithoutSpaces, 4, ' ')).toBe('3529 5453 3558 727')
+  });
+  test('if handleNumCartao with non-valid digits filter all the non-numbers digits and return empty', () => {
+    const numWithoutSpaces = onlyNumbers('aabb-ccdd');
+    expect(numWithoutSpaces).toHaveLength(0);
+  });
+
+  test('if handleVencimento with valid digits filter all the non-numbers digits and add a slash after 2 digits', () => {
+    const numWithoutSlash = onlyNumbers('12a30');
+    expect(addChar(numWithoutSlash, 2, '/')).toBe('12/30');
+  });
+  test('if handleVencimento with non-valid digits filter all the non-numbers digits and return empty', () => {
+    const numWithoutSlash = onlyNumbers('aa/bb');
+    expect(numWithoutSlash).toHaveLength(0);
+  });
+
+  it('should check if there\'s already a card with the same card number', () => {
+    const cards = [{
+      number: "3529 5435 3355 8727",
+    }];
+    const checkDuplicates = (cardNumber: string) => cards.find(card => card.number === cardNumber);
+    expect(checkDuplicates('3529 5435 3355 8727')).toBeTruthy();
+  });
+  test.todo('handleForward without cardToShow should run checkDuplicates and, in case of a negative return, should create a new card');
+  test.todo('handleForward with cardToShow should redirect the user to the card list page');
 })
